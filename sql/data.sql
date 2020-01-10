764e160d282c2c454f25ae8f3fbbfb9ed61f0a72
anil17081998 <anilwynne@gmail.com>
2020-01-10 14:17:40 +0530

    SQL

--- sql/data.sql
+++ sql/data.sql
@@ -1,1 +1,77 @@
 -- Include table data insertion, updation, deletion and select scripts
+
+
+-- -----------------------------------------------------
+-- Admin Movie List
+-- -----------------------------------------------------
+insert into movie
+values
+(1,'Avatar',2787965087,'Yes','2017-03-15','Science Fiction','Yes'),
+(2,'The Avengers',1518812988,'Yes','2017-12-23','Superhero','No'),
+(3,'Titanic',2187463944,'Yes','2018-08-21','Romance','No'),
+(4,'Jurassic World',1671713208,'No','2017-07-02','Science Fiction','Yes'),
+(5,'Avengers End Game',2750760348,'Yes','2022-11-02','Superhero','Yes');
+
+
+select * from movie;
+
+-- -----------------------------------------------------
+-- Update Movie List
+-- -----------------------------------------------------
+
+update movie
+set mov_title='Iron Man',
+mov_box_office=150989777,mov_genre='Superhero'
+where mov_id=1;
+
+-- -----------------------------------------------------
+-- Customer Movie List
+-- -----------------------------------------------------
+
+
+select mov_title,mov_box_office,mov_genre,mov_has_teaser
+from movie
+where mov_active='Yes' and 
+mov_date_of_launch <= current_date();
+
+
+-- -----------------------------------------------------
+-- Adding User
+-- -----------------------------------------------------
+
+insert into user 
+values(1,'anil123'),
+(2,'user007'),(3,'vibu');
+
+select * from user;
+select * from favorite;
+
+-- -----------------------------------------------------
+-- Favorites
+-- -----------------------------------------------------
+
+insert into favorite (fav_us_id,fav_mov_id) 
+values(2,1),(2,2),(2,5),(2,1),(1,1),(1,4);
+
+
+
+select mov_title,mov_box_office,mov_genre 
+from movie
+inner join favorite on fav_mov_id=mov_id
+where fav_us_id=2; 
+
+-- -----------------------------------------------------
+-- Delete Favorite
+-- -----------------------------------------------------
+
+
+delete from favorite 
+where fav_us_id=2 and fav_mov_id=1 limit 1; 
+
+-- -----------------------------------------------------
+-- No of Favorites
+-- -----------------------------------------------------
+select count(mov_title) as No_of_favorites from movie
+inner join favorite on fav_mov_id=mov_id
+where fav_us_id=2; 
+
--- sql/schema-creation.sql
+++ sql/schema-creation.sql
@@ -1,3 +1,69 @@
 -- 1. Design the schema for movie cruiser in MySQL Workbench
 -- 2. Generate the SQL schema script in MySQL Workbench
 -- 3. Paste the generated the SQL code here
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema truyum
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema truyum
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `moviecruiser` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `moviecruiser` ;
+
+-- -----------------------------------------------------
+-- Table `truyum`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `moviecruiser`.`user` (
+  `us_id` INT NOT NULL AUTO_INCREMENT,
+  `us_name` VARCHAR(60) NULL,
+  PRIMARY KEY (`us_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `truyum`.`menu_item`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `moviecruiser`.`movie` (
+  `mov_id` INT NOT NULL AUTO_INCREMENT,
+  `mov_title` VARCHAR(100) NULL,
+  `mov_box_office` DECIMAL(15) NULL,
+  `mov_active` VARCHAR(3) NULL,
+  `mov_date_of_launch` DATE NULL,
+  `mov_genre` VARCHAR(45) NULL,
+  `mov_has_teaser` VARCHAR(3) NULL,
+  PRIMARY KEY (`mov_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `truyum`.`cart`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `moviecruiser`.`favorite` (
+  `fav_id` INT NOT NULL AUTO_INCREMENT,
+  `fav_us_id` INT NULL,
+  `fav_mov_id` INT NULL,
+  PRIMARY KEY (`fav_id`),
+  INDEX `fav_us_fk_idx` (`fav_us_id` ASC),
+  INDEX `fav_mov_fk_idx` (`fav_mov_id` ASC),
+  CONSTRAINT `fav_us_fk`
+    FOREIGN KEY (`fav_us_id`)
+    REFERENCES `moviecruiser`.`user` (`us_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fav_mov_fk`
+    FOREIGN KEY (`fav_mov_id`)
+    REFERENCES `moviecruiser`.`movie` (`mov_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
